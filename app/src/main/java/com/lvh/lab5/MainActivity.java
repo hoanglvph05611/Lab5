dde42032b0d615fe40706fe265d8ab0471cabdb3
hoanglv <levanhoang0699@gmail.com>
2019-07-31 17:41:19 +0700

    update

--- app/src/main/java/com/lvh/lab5/MainActivity.java
+++ app/src/main/java/com/lvh/lab5/MainActivity.java
@@ -1,16 +1,21 @@
 package com.lvh.lab5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText etMsg;
     private RecyclerView mList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +24,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+      //  getSupportActionBar().setTitle("Lê Hoàng");
+        toolbar.setContentInsetStartWithNavigation(10);
+        toolbar.setNavigationIcon(R.drawable.drawer);
+
+
+       // toolbar.setSubtitle("last active: 10 sec ago");
+
+
         mList = findViewById(R.id.mList);
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
         mList.setLayoutManager(mLayoutManager);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.videoCall:
+                Toast.makeText(this, "Video call", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.call:
+                Toast.makeText(this, "Call", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.setting:
+                Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
